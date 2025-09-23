b0c9c621807953f9653becb27b0ac9282e3316ab
Petar Gutov <pgutov@gmail.com>
2025-09-23 11:07:21 +0200

    endpoints in notification controller changed

--- src/main/java/app/web/dto/EmailResponse.java
+++ src/main/java/app/web/dto/EmailResponse.java
@@ -8,6 +8,11 @@ import java.time.LocalDateTime;
 public record EmailResponse (String recipient, String subject, LocalDateTime sentOn) {
 
     public static EmailResponse fromEmailEntity(Email email) {
-        return new EmailResponse(email.getRecipient(), email.getSubject(), email.getSentOn());
+
+        return new EmailResponse(
+                email.getRecipient(),
+                email.getSubject(),
+                email.getSentOn()
+        );
     }
 }
