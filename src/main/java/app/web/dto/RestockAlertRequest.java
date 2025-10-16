d4b87b92ec3ae27e63c15fbef0d183a5965e33e7
Petar Gutov <pgutov@gmail.com>
2025-10-16 11:01:13 +0200

    validation added to the restock alert dto

--- src/main/java/app/web/dto/RestockAlertRequest.java
+++ src/main/java/app/web/dto/RestockAlertRequest.java
@@ -1,13 +1,17 @@
 package app.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RestockAlertRequest {
 
+    @NotBlank
     private String recipient;
 
+    @NotBlank
     private String subject;
 
+    @NotBlank
     private String body;
 }
